94c17a1e1823804a8480da44c39dea7b1f06cdcc
JUNGJUNILL <dala1207@naver.com>
2019-06-10 15:36:21 +0900

    asdasd

--- src/com/java/basic/P010Test_isNumer.java
+++ src/com/java/basic/P010Test_isNumer.java
@@ -26,7 +26,7 @@ public class P010Test_isNumer {
 	static boolean isStringDouble(String str){
 	
 			try{
-				//Á¤ÁØÀÏÂ¯dddss
+				//Á¤ÁØÀÏÂ¯dddssdddd
 				sdfsdf
 				Double.parseDouble(str); 
 				return true; 
